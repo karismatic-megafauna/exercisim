aa67166a819177b83c74a1cd351817db364c6fdd
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-11-28 20:58:14 -0800

    Finished refactor. However it still relys on order :(

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -13,7 +13,7 @@ Triangle.prototype.isEquilateral = function(){
   return false;
 }
 
-Triangle.prototype.isScalene= function(){
+Triangle.prototype.isScalene = function(){
   if(this.a !== this.b && this.b !== this.c){
     this.triangleType = 'scalene';
     return this.triangleType;
@@ -21,41 +21,55 @@ Triangle.prototype.isScalene= function(){
   return false;
 }
 
-Triangle.prototype.kind = function() {
-  var sides = [this.a, this.b, this.c];
+Triangle.prototype.isIsosceles = function(){
+  if (this.a !== this.b && this.b === this.c ||
+      this.a !== this.b && this.a === this.c ||
+      this.a !== this.c && this.a === this.b) {
+    this.triangleType = 'isosceles';
+  }
+  return false;
+}
 
+Triangle.prototype.isValidTriangle = function(){
+  // Triangle Inequality
   var ab = this.a + this.b;
   var bc = this.b + this.c;
   var ac = this.a + this.c;
-  
+
+  if ( ab < this.c || bc < this.a || ac < this.b ) {
+    throw 'Violates Triangle Inequality';
+  }
+  return true;
+}
+
+Triangle.prototype.isPositive = function(){
+  var sides = [this.a, this.b, this.c];
+
   // No negative numbers and total
   var total = sides.reduce(function(mem, obj){
     if (Math.sign(obj) === -1){
       throw 'No negative numbers';
     }
-    return mem + obj; 
+    return mem + obj;
   });
 
   if ( total === 0 ) {
     throw 'Triangle should be greater than zero';
   }
 
-  // Triangle Inequality
-  if ( ab < this.c || bc < this.a || ac < this.b ) {
-    throw 'Violates Triangle Inequality';
-  }
+  return true;
+}
+
+
+Triangle.prototype.kind = function() {
+  // Make sure it is valid triangle
+  this.isPositive();
+  this.isValidTriangle();
 
   // Check triangles
-  // order matters, i wonder if i can make that not a thing
   this.isEquilateral();
   this.isScalene();
-  if ( this.a !== this.b && this.b === this.c ) {
-    this.triangleType = 'isosceles';
-  } else if ( this.a !== this.b && this.a === this.c ) {
-    this.triangleType = 'isosceles';
-  } else if ( this.a !== this.c && this.a === this.b ) {
-    this.triangleType = 'isosceles';
-  }
+  this.isIsosceles();
   return this.triangleType;
 };
 
