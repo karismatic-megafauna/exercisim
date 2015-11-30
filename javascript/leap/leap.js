312f6931645bc18490ec09f309059a10e056cb61
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-11-29 22:31:35 -0800

    Wee leap is duuun

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,11 +1,17 @@
-'use strict'
-
 var Year = function(year) {
     this.year = year;
 };
 
 Year.prototype.isLeap = function() {
+  var four = this.year % 4;
+  var hundred = this.year % 100;
+  var fourHundred = this.year % 400;
+
+  if ( four === 0 && hundred !== 0 || fourHundred === 0) {
+    return true;
+  } else {
     return false;
+  }
 };
 
 module.exports = Year;
