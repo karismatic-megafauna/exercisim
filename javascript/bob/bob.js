eb250f47af02468c23917a9480de2fde5336fe51
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-12-05 18:40:33 -0800

    testing

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -3,7 +3,6 @@ var Bob = function() {};
 // going to make some functions that detect the following:
 // what kind of punctuation does it have at the end? returns ( . ? ! )
 // does it have words that are all caps?
-// empty string 
 
 Bob.prototype.hasLowerCase = function(str) {
   return (/[a-z]/.test(str)) ? true : false; 
@@ -11,14 +10,6 @@ Bob.prototype.hasLowerCase = function(str) {
 
 Bob.prototype.getPunctuationMark = function(str) {
   return str.slice(-1);
-}
-
-Bob.prototype.onlyNumbers = function(str) {
-  if ( str.match(/\d+/g) ) {
-    return str.match(/\d+/g);
-  } else {
-    return [''];
-  }
 }
 
 Bob.prototype.hey = function(input) {
