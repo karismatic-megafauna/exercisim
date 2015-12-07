a87b910d9db34cc8def446b407a3d9822af3ded8
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-12-06 23:50:35 -0800

    Trying this crazy accumulator method.

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,4 +1,8 @@
-var Bob = function() {};
+var Bob = function() {
+  this.question = 0;
+  this.statement = 0;
+  this.exclimation = 0;
+};
 
 // going to make some functions that detect the following:
 // what kind of punctuation does it have at the end? returns ( . ? ! )
@@ -8,16 +12,24 @@ Bob.prototype.hasLowerCase = function(str) {
   return (/[a-z]/.test(str)) ? true : false; 
 }
 
-Bob.prototype.getPunctuationMark = function(str) {
+Bob.prototype.getLastChar = function(str) {
   return str.slice(-1);
 }
 
 Bob.prototype.hey = function(input) {
-  if ( this.getPunctuationMark(input) === '!' &&  this.hasLowerCase(input) === false) {
+  var inputArry = input.split('').reduce(function(mem, obj){
+    //
+    // if (obj === ' '){
+    //   return mem 
+    // }
+    return mem + obj;
+  });
+  console.log(inputArry, "-----------")
+  if ( this.getLastChar(input) === '!' &&  this.hasLowerCase(input) === false) {
     return 'Whoa, chill out!';
-  } else if ( this.getPunctuationMark(input) === '?' && this.hasLowerCase(input) === false) {
+  } else if ( this.getLastChar(input) === '?' && this.hasLowerCase(input) === false) {
     return 'Whoa, chill out!';
-  } else if ( this.getPunctuationMark(input) === '?') {
+  } else if ( this.getLastChar(input) === '?') {
     return 'Sure.';
   } else if (input) {
     return 'Whatever.';
