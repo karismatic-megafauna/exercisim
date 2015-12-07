f214e38350c337864571861d99231ea5fc3bc109
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-12-07 00:18:37 -0800

    I hate how i am doing this. can't think of any other way

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,7 +1,4 @@
 var Bob = function() {
-  this.question = 0;
-  this.statement = 0;
-  this.exclimation = 0;
 };
 
 // going to make some functions that detect the following:
@@ -9,7 +6,7 @@ var Bob = function() {
 // does it have words that are all caps?
 
 Bob.prototype.hasLowerCase = function(str) {
-  return (/[a-z]/.test(str)) ? true : false; 
+  return (/[a-z]/.test(str)) ? true : false;
 }
 
 Bob.prototype.getLastChar = function(str) {
@@ -17,16 +14,30 @@ Bob.prototype.getLastChar = function(str) {
 }
 
 Bob.prototype.hey = function(input) {
-  var inputArry = input.split('').reduce(function(mem, obj){
-    //
-    // if (obj === ' '){
-    //   return mem 
-    // }
-    return mem + obj;
+  var question = 0;
+  var statement = 0;
+  var exclimation = 0;
+  var isNumber = 0;
+
+  input.split('').map(function(obj){
+    if (obj === '?'){
+      question = question + 1;
+    }
+    if (obj === '!'){
+      exclimation = exclimation + 1;
+    }
+    if ( obj === '.') {
+      statement = statement + 1;
+    }
+    if ( /^\d+$/.test(obj) ) {
+      isNumber = isNumber + 1;
+    }
   });
-  console.log(inputArry, "-----------")
+
   if ( this.getLastChar(input) === '!' &&  this.hasLowerCase(input) === false) {
     return 'Whoa, chill out!';
+  } else if ( (input.length - 1 === isNumber )) {
+    return 'Sure.';
   } else if ( this.getLastChar(input) === '?' && this.hasLowerCase(input) === false) {
     return 'Whoa, chill out!';
   } else if ( this.getLastChar(input) === '?') {
