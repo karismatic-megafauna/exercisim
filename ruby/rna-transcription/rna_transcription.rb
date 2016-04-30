33b5411396aa584c94741742b30f80cb73012019
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-04-30 15:27:35 -0700

    Finish RNA transcription

---
+++ ruby/rna-transcription/rna_transcription.rb
@@ -0,0 +1,20 @@
+class Complement
+  VERSION=3
+  def self.of_dna(str)
+    result=''
+    str.chars.to_a.map do |char|
+      if char == 'C'
+        result += 'G'
+      elsif char == 'G'
+        result += 'C'
+      elsif char == 'T'
+        result += 'A'
+      elsif char == 'A'
+        result += 'U'
+      else
+        throw ArgumentError
+      end
+    end
+    result
+  end
+end
