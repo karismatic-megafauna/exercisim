c66a8b662ed75c1f97041e74dfa9d828f0b05ffc
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-05-01 15:23:24 -0700

    Finish Sieve

---
+++ ruby/sieve/sieve.rb
@@ -0,0 +1,33 @@
+require 'set'
+class Sieve
+  def initialize(num)
+    @num = num
+    @range= [*2..num]
+  end
+
+  #  take the next available unmarked number in your list (it is prime)
+  #  mark all the multiples of that number (they are not prime)
+  def primes
+    if @num < 2
+      return []
+    end
+    primeList = @range
+    killList = Set.new []
+    primeList.each { |i|
+      killList.merge(getMultiples(i))
+    }
+
+    (primeList.to_set - killList).to_a
+  end
+
+  def getMultiples(n)
+    counter = 1
+    multiples = [n]
+    while multiples.last < @range.last
+      counter += 1
+      multiples.push(n*counter)
+    end
+    multiples.shift
+    multiples.to_set
+  end
+end
