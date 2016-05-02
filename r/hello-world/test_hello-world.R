59d531a0e73f710d6259389ff7386e6efbc23529
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-05-02 08:48:38 -0700

    Fetch got a lot of things

---
+++ r/hello-world/test_hello-world.R
@@ -0,0 +1,16 @@
+source('./hello_world.R')
+suppressPackageStartupMessages({ require(testthat) })
+
+test_that("no name", {
+  expect_equal(hello_world(), "Hello, World!")
+})
+
+test_that("sample name", {
+  expect_equal(hello_world("Alice"), "Hello, Alice!")
+})
+
+test_that("other sample name", {
+  expect_equal(hello_world("Bob"), "Hello, Bob!")
+})
+
+print("All tests passed!")
