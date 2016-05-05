4470316c20f44b48e0d96123b80bd887d823a2e0
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-05-04 22:54:31 -0700

    Add comments to roman numerals

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,13 +1,47 @@
 class Fixnum
   VERSION=1
   def to_roman
-    if ( self / 3 ) < 1
-      makeSingle(self)
+    numeral=[]
+    number=self
+    magnitude = Math.log10(number).floor
+
+    if magnitude == 0
+      numeral.push(makeNumerals('I','V'))
+    elsif magnitude == 1
+      numeral.push(makeNumerals('X','L'))
+    elsif magnitude == 2
+      numeral.push(makeNumerals('C','D'))
+    elsif magnitude == 3
+      numeral.push(makeNumerals('M',''))
     end
+    numeral.join('')
   end
 
-  def makeSingle(num)
-    (0..num).each {|i| 'I'}
+  def makeNumerals(one, five)
+    Array.new(num, 'I')
   end
 end
+
+# IIII
+# IV
+
+## Get group
+# Math.log10(num).floor
+# this will return which level it is
+# use this to switch between IV XL and CD
+
+## rules
+# M = 1000
+##
+# D = 500
+# C = 100
+###
+# L 50
+# X = 10
+# ##
+# V = 5
+# I = 1
+# ##
+# left = minus
+# right = add
 
